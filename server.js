3182cadbdac4770b3cd3f2169c3159c5c7ae53d5
Htet Aung <htetaung20025q@example.com>
2026-07-05 17:20:10 +0630

    Fix CORS policy error

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.json());
 
 // API လမ်းကြောင်း သတ်မှတ်ခြင်း (http://localhost:5000/api/metrics ဖြင့် ခေါ်ယူနိုင်မည်)
